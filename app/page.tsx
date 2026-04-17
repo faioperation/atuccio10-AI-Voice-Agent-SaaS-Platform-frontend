424f7eff98a59112491518398038bb181014d89a
samio11 <samiohasan6@gmail.com>
2026-04-17 07:13:21 +0600

    project Dashboard done

---
+++ app/page.tsx
@@ -0,0 +1,30 @@
+import Advantage from "@/components/Home/Advantage";
+import CTA from "@/components/Home/CTA";
+import Features from "@/components/Home/Features";
+import Footer from "@/components/Home/Footer";
+import Hero from "@/components/Home/Hero";
+import HowItWorks from "@/components/Home/HowItWorks";
+import Integrations from "@/components/Home/Integrations";
+import Metrics from "@/components/Home/Metrics";
+import Navbar from "@/components/Home/Navbar";
+import Pricing from "@/components/Home/Pricing";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <Navbar />
+      <main>
+        <Hero />
+        <Features />
+        <Advantage />
+        <HowItWorks />
+        <Metrics />
+        <Pricing />
+        <Integrations />
+        <CTA />
+      </main>
+      <Footer />
+    </>
+  );
+}
